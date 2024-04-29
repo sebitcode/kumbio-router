e5e856857b1c44634753728f310cb0b247fb4843
sebitcode <sebitcode@gmail.com>
2024-04-28 21:11:45 -0500

    server

--- deploys/production/traefik/back/production.sh
+++ deploys/production/traefik/back/production.sh
@@ -3,7 +3,5 @@ cd ../../../../yml/production/traefik/back
 echo "BEGIN DEPLOYMENT"
 docker compose -f production.yml build
 docker compose -f production.yml down --remove-orphans
-echo "REMOVE EXTRA CONTAINERS"
-docker rm -v -f $(docker ps -qa)
 docker compose -f production.yml up -d
 echo "FINISH DEPLOYMENT"
