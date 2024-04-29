8b2ac9a86649f5a0db0e8a95051ed9c297db31ba
sebitcode <sebitcode@gmail.com>
2024-04-28 21:00:38 -0500

    server

--- deploys/staging/traefik/staging.sh
+++ deploys/staging/traefik/staging.sh
@@ -7,11 +7,3 @@ echo "REMOVE EXTRA CONTAINERS"
 docker rm -v -f $(docker ps -qa)
 docker compose -f staging.yml up -d
 echo "FINISH DEPLOYMENT"
-echo "START REMOVING ALL USELESS IMAGES"
-docker rmi $(docker images -f dangling=true -q)
-echo "END REMOVING ALL USELESS IMAGES"
-echo "START REMOVING ALL USELESS VOLUMES"
-docker volume rm `docker volume ls -q -f dangling=true`
-echo "END REMOVING ALL USELESS VOLUMES"
-docker system prune --force
-echo "CLEAN DOCKER"
